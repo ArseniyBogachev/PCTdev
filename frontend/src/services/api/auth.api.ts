95dc8d9989254e9abebbe4300becaca7d3e6ea6f
kuwsh1n <senya.bogachev@mail.ru>
2024-11-11 03:33:39 +0300

    fix update me

--- frontend/src/services/api/auth.api.ts
+++ frontend/src/services/api/auth.api.ts
@@ -76,7 +76,7 @@ async function updateMeApi(
     }
 ) {
     try {
-        const response = await axios.put('http://127.0.0.1:8000/api/v1/auth/users/me/', 
+        const response = await axios.patch('http://127.0.0.1:8000/api/v1/auth/users/me/', 
             data,
             {
                 headers: {
